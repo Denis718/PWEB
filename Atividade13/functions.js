31115f6fdeaae1fde6d297d12e34f9d7a1dca16c
Denis718 <70110877+Denis718@users.noreply.github.com>
2020-11-04 23:57:04 -0300

    Add files via upload

---
+++ Atividade13/functions.js
@@ -0,0 +1,11 @@
+function seleciona(){
+    var curso = document.getElementById('curso')
+
+    alert(`Deseja abrir a página do curso ${curso.options[curso.selecteIndex].value}`)
+}
+   
+//element.options[element.selectedIndex].value
+
+
+
+//location=this.value
